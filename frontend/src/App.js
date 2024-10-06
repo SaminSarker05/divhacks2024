c0e7774fbd4be5eb8ecce5e2f5f78f4e4bf6f139
Samin Sarker <saminsarker05@gmail.com>
2024-10-05 21:17:18 -0400

    cleaning react boiler files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
