ac306598975f0a168a748b7faf718f2d18c4ed9d
Samin Sarker <saminsarker05@gmail.com>
2024-10-05 21:47:25 -0400

    backend frontend connection made

--- backend/base.py
+++ backend/base.py
@@ -1,6 +1,13 @@
 from flask import Flask 
+from flask_cors import CORS
 
 app = Flask(__name__)
-@app.route('/home')
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})  # Allow requests from your React frontend
+
+@app.route('/home', methods=['GET'])
 def home():
-  return {"name": "hello world"}
+  response_body = {
+    "name": "Nagato",
+    "about" :"Hello! I'm a full stack developer that loves python and javascript"
+  }
+  return response_body, 200
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,42 @@
-import './App.css';
-import { useState } from 'react';
+import { useState } from 'react'
 import axios from "axios";
+import './App.css';
 
 function App() {
+
+   // new line start
+  const [profileData, setProfileData] = useState(null)
+
+  function getData() {
+    axios({
+      method: "GET",
+      url:"http://127.0.0.1:5000/home",
+    })
+    .then((response) => {
+      const res =response.data
+      setProfileData(({
+        profile_name: res.name,
+        about_me: res.about}))
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+        }
+    })}
+    //end of new line 
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>To get your profile details: </p>
+        <button onClick={getData}>Click me</button>
+        {profileData && <div>
+              <p>Profile name: {profileData.profile_name}</p>
+              <p>About me: {profileData.about_me}</p>
+            </div>
+        }
       </header>
     </div>
   );
